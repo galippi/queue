da21d73532821c59559ea6ba45797680a7fe6d85
Gabor Liptak <liptakgabor@freemail.hu>
2021-01-08 16:11:07 +0100

    Const data and volatile data are separated

--- queue.c
+++ queue.c
@@ -14,13 +14,11 @@ INLINE tQueueIdx queueArrayIdx(const tQueue *q, tQueueIdx idx)
     return ((idx < q->size) ? idx : (idx - q->size));
 }
 
-int8_t queueInit(tQueue *q, tQueueData *dataPtr, tQueueIdx size)
+int8_t queueInit(const tQueue *q)
 {
-  q->in = 0;
-  q->out = 0;
-  q->dataPtr = dataPtr;
-  q->size = size;
-  if (size >= (1 << (sizeof(tQueueIdx) * 7)))
+  q->idxPtr->in = 0;
+  q->idxPtr->out = 0;
+  if (q->size >= (1 << (sizeof(tQueueIdx) * 7)))
   {
       QUEUE_CONFIG_ERROR_HANDLER(q);
       return 1; /* the bit size of tQueueIdx is not enough to store the data */
@@ -30,13 +28,13 @@ int8_t queueInit(tQueue *q, tQueueData *dataPtr, tQueueIdx size)
 
 int8_t queueIsEmpty(const tQueue *q)
 {
-  return (q->in == q->out);
+  return (q->idxPtr->in == q->idxPtr->out);
 }
 
 tQueueIdx queueGetNum(const tQueue *q)
 {
-  tQueueIdx num = q->in - q->out;
-  if (q->in >= q->out)
+  tQueueIdx num = q->idxPtr->in - q->idxPtr->out;
+  if (q->idxPtr->in >= q->idxPtr->out)
   {
       if (num > q->size)
           num -= q->size;
@@ -57,16 +55,16 @@ int8_t queueIsFull(const tQueue *q)
   return (num == q->size);
 }
 
-int8_t queuePut(tQueue *q, tQueueData data)
+int8_t queuePut(const tQueue *q, tQueueData data)
 {
   if (queueIsFull(q))
     return 1;
-  q->dataPtr[queueArrayIdx(q, q->in)] = data;
-  q->in = queueNextIdx(q, q->in);;
+  q->dataPtr[queueArrayIdx(q, q->idxPtr->in)] = data;
+  q->idxPtr->in = queueNextIdx(q, q->idxPtr->in);;
   return 0;
 }
 
-tQueueIdx queueWrite(tQueue *q, const tQueueData *data, tQueueIdx num)
+tQueueIdx queueWrite(const tQueue *q, const tQueueData *data, tQueueIdx num)
 {
     tQueueIdx written = 0;
     while (num != 0)
@@ -80,16 +78,16 @@ tQueueIdx queueWrite(tQueue *q, const tQueueData *data, tQueueIdx num)
     return written;
 }
 
-int8_t queueGet(tQueue *q, tQueueData *data)
+int8_t queueGet(const tQueue *q, tQueueData *data)
 {
   if (queueIsEmpty(q))
     return 1;
-  *data = q->dataPtr[queueArrayIdx(q, q->out)];
-  q->out = queueNextIdx(q, q->out);
+  *data = q->dataPtr[queueArrayIdx(q, q->idxPtr->out)];
+  q->idxPtr->out = queueNextIdx(q, q->idxPtr->out);
   return 0;
 }
 
-tQueueData queueGetData(tQueue *q)
+tQueueData queueGetData(const tQueue *q)
 {
   tQueueData data;
   if (queueGet(q, &data))
@@ -100,7 +98,7 @@ tQueueData queueGetData(tQueue *q)
   return data;
 }
 
-tQueueIdx queueRead(tQueue *q, tQueueData *data, tQueueIdx num)
+tQueueIdx queueRead(const tQueue *q, tQueueData *data, tQueueIdx num)
 {
     tQueueIdx readNum = 0;
     while (num != 0)
