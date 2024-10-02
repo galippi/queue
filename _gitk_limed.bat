992f6b209ff11414833f52681586055ff01db357
Gabor Liptak <liptakgabor@freemail.hu>
2024-10-02 19:28:07 +0200

    gitk script of limed is updated

--- _gitk_limed.bat
+++ _gitk_limed.bat
@@ -1,5 +1,3 @@
-::set path=C:\Programs\Git\bin\;%PATH%
-set path=C:\Programs\cygwin\bin\;%PATH%
-set HOME=D:\Gabor
-::set HOME=C:\Dokumentumok\Gabor\cygwin
-gitk
+@set path=C:\Programok\Git\bin;%PATH%
+@set HOME=D:\Gabor
+C:\Programok\Git\cmd\git-gui.exe
