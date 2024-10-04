dd547b994d1bb1a3346b1cf50149654fb4f3f13d
Gabor Liptak <liptakgabor@freemail.hu>
2024-10-04 21:06:46 +0200

    queueIsEmpty is made inlined assert_lw is added and used queueWrite is speed optimized

--- queue_conf.h
+++ queue_conf.h
@@ -2,6 +2,7 @@
 #define _QUEUE_CONF_H_
 
 #include <stdint.h>
+#include <assert.h>
 
 #define INLINE static inline
 
@@ -14,5 +15,6 @@ void queueEmptyErrorHandler(const tQueue *q);
 #define QUEUE_EMPTY_ERROR_HANDLER(q) queueEmptyErrorHandler(q)
 
 #define QUEUE_CONFIG_ERROR_HANDLER(q) /* do nothing */
+#define assert_lw(level, cond) assert(cond)
 
 #endif /* _QUEUE_CONF_H_ */
